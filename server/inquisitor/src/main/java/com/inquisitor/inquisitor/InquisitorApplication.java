455fd190c8592b934135f257e80b9b039c82bf8c
Anushwar Sharma <Anushwarsharma07@gmail.com>
2023-10-14 13:04:48 +0530

    feat: initialise spring application

---
+++ server/inquisitor/src/main/java/com/inquisitor/inquisitor/InquisitorApplication.java
@@ -0,0 +1,13 @@
+package com.inquisitor.inquisitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquisitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquisitorApplication.class, args);
+	}
+
+}
---
+++ server/inquisitor/src/test/java/com/inquisitor/inquisitor/InquisitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inquisitor.inquisitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquisitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
