c44c08955a38e00f65d4909ae203f284bef0fa1c
Anushwar Sharma <Anushwarsharma07@gmail.com>
2023-10-19 19:56:39 +0530

    feat: create chart component

---
+++ client/src/components/ui/chart.tsx
@@ -0,0 +1,45 @@
+import { Line, Bar, Pie } from '@ant-design/plots';
+import type { LineConfig, BarConfig, PieConfig } from '@ant-design/plots';
+
+interface ChartProps {
+  xField: string;
+  yField: string;
+  data: unknown;
+}
+
+const Chart = ({ xField, yField, data }: ChartProps) => {
+  const config = {
+    data,
+    padding: 'auto',
+    style: {
+      backgroundColor: 'white',
+    },
+  };
+
+  const generateGraph = (type: string) => {
+    const val = type.toLowerCase();
+    switch (val) {
+      case 'pie':
+        return (
+          <Pie
+            {...(config as PieConfig)}
+            appendPadding={10}
+            angleField={yField}
+            colorField={xField}
+          />
+        );
+      case 'bar':
+        return (
+          <Bar {...(config as BarConfig)} xField={xField} yField={yField} />
+        );
+      default:
+        return (
+          <Line {...(config as LineConfig)} xField={xField} yField={yField} />
+        );
+    }
+  };
+
+  return generateGraph('pie');
+};
+
+export default Chart;
