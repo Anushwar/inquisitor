551a56cdc34ee9c8ad88b1cf25b63bd2a4d5ecef
Anushwar Sharma <Anushwarsharma07@gmail.com>
2023-10-20 08:32:45 +0530

    chore: add gptfunction constructor

--- server/inquisitor/src/main/java/com/inquisitor/inquisitor/dto/GPTFunction.java
+++ server/inquisitor/src/main/java/com/inquisitor/inquisitor/dto/GPTFunction.java
@@ -1,5 +1,6 @@
 package com.inquisitor.inquisitor.dto;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -23,8 +24,32 @@ public class GPTFunction {
         public static class Property {
             private String type;
             private String description;
+            private List<String> enumValues;
 
         }
     }
 
+    public GPTFunction() {
+        this.name = "generate_graph";
+        this.description = "Based on the input CSV Data, understand the data and determine the graph to be used to represent this data. Data can be can be Categorical, Numerical, Temporal. unit is only applicable for Numerical type. Can be number, ratio, percentage or currency. Only generate a graph config with xfield, yfield and graph_name from ant design graphs.";
+        this.parameters = new Parameters();
+        this.parameters.setType("object");
+
+        Parameters.Property xfieldProperty = new Parameters.Property();
+        xfieldProperty.setType("string");
+        xfieldProperty.setDescription("Name of the CSV Column that goes on x-axis.");
+
+        Parameters.Property yfieldProperty = new Parameters.Property();
+        yfieldProperty.setType("string");
+        yfieldProperty.setDescription("Name of the CSV Column that goes on y-axis.");
+
+        Parameters.Property graphNameProperty = new Parameters.Property();
+        graphNameProperty.setType("string");
+        graphNameProperty.setEnumValues(Arrays.asList("Line", "Pie", "Bar"));
+
+        this.parameters.setProperties(Map.of(
+                "xfield", xfieldProperty,
+                "yfield", yfieldProperty,
+                "graph_name", graphNameProperty));
+    }
 }
